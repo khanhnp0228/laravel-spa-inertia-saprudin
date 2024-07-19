f4acc12c51018767a78779caf6b774a3a1d2987b
Khanh Nguyen <khanh@propolife.co.jp>
2024-07-19 15:44:34 +0700

    Deleting resources

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -79,6 +79,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return redirect()->route('products.index');
     }
 }
--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link, router } from '@inertiajs/vue3';
 
 defineProps({
     products: {
@@ -8,6 +8,14 @@ defineProps({
         required: true
     }
 })
+
+const deleteRow = (id) => {
+    if(window.confirm('Are you delete ?')){
+        router.delete(route('products.destroy', id),{
+            preserveScroll: true
+        });
+    }
+}
 
 </script>
 
@@ -62,7 +70,7 @@ defineProps({
                                 <td class="px-6 py-4 space-x-2">
                                     <Link :href="route('products.show', product.id)" class="font-medium text-gray-600 hover:underline">Show</Link>
                                     <Link :href="route('products.edit', product.id)" class="font-medium text-blue-600 hover:underline">Edit</Link>
-                                    <a href="#" class="font-medium text-red-600 hover:underline">Delete</a>
+                                    <a href="#" class="font-medium text-red-600 hover:underline" @click.prevent="deleteRow(product.id)">Delete</a>
                                 </td>
                             </tr>
                         </tbody>
