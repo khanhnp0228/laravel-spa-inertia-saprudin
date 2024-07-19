ef44d6a19ec9c67254274fd9b876ce0263692cef
Khanh Nguyen <khanh@propolife.co.jp>
2024-07-19 09:26:24 +0700

    Working with Accessors and Muttators

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -38,10 +38,10 @@ class StoreProductRequest extends FormRequest
         ];
     }
 
-    public function prepareForValidation()
-    {
-        $this->merge([
-            'price' => $this->price ? $this->price * 100 : null
-        ]);
-    }
+    // public function prepareForValidation()
+    // {
+    //     $this->merge([
+    //         'price' => $this->price ? $this->price * 100 : null
+    //     ]);
+    // }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -18,5 +19,14 @@ class Product extends Model
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    /* Accessors and Mutators */
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
     }
 }
